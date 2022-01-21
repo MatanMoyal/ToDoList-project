53ca8cbf64f95f529cc177f117edca6bd228df46
MatanMoyal <matanmoyal2@gmail.com>
2022-01-21 22:29:45 +0200

    make it better

--- back.js
+++ back.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/css', express.static(path.join(__dirname, "/public/css")));
 // app.set('views', path.join(__dirname, '../views'))
-app.set('views', '../views')
+app.set('views','./views')
 app.set('view engine', 'ejs');
 
 // inital system
